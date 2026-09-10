ef412f73e205eb722d2e52119c8823bc709e4571
Mykxo <ik_h291@outlook.com>
2026-09-10 12:42:18 -0400

    Enlevement de lombok

--- src/main/java/com/lacouf/rsbjwt/model/UserApp.java
+++ src/main/java/com/lacouf/rsbjwt/model/UserApp.java
@@ -1,45 +1,48 @@
 package com.lacouf.rsbjwt.model;
 
 import com.lacouf.rsbjwt.model.auth.Credentials;
-import com.lacouf.rsbjwt.model.auth.Role;
 import jakarta.persistence.*;
-import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-public abstract class UserApp  {
+public abstract class UserApp {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
-
     private String lastName;
 
     @Embedded
     private Credentials credentials;
 
-    public String getEmail(){
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
         return credentials.getEmail();
     }
 
-    public String getPassword(){
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public String getPassword() {
         return credentials.getPassword();
     }
 
-    public Role getRole(){
-        return credentials.getRole();
-    }
-
-
-    public Collection<? extends GrantedAuthority> getAuthorities(){
+    public Collection<? extends GrantedAuthority> getAuthorities() {
         return credentials.getAuthorities();
     }
 }
