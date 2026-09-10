5073c5cdbcd03fdf564a5d19fb31d4e62e9babd8
Mykxo <ik_h291@outlook.com>
2026-09-10 15:56:13 -0400

    Creation de la classe abstraite Utilisateur

---
+++ src/main/java/com/lacouf/rsbjwt/model/Utilisateur.java
@@ -0,0 +1,15 @@
+package com.lacouf.rsbjwt.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+}
