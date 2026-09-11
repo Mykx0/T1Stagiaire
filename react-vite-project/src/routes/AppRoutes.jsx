ab59c7785bb2dc1a4c50a053f17589bd4116261d
TNKDieguexLapLinux <diegoaguilar20040303@gmail.com>
2026-09-11 19:55:19 -0400

    mise à jour des routes

--- react-vite-project/src/routes/AppRoutes.jsx
+++ react-vite-project/src/routes/AppRoutes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import Connexion from "../components/auth/connexion.jsx";
+import RegisterPage from "../components/auth/RegisterPage.jsx";
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={"/connexion"} element={<Connexion />} />
-                <Route path={"*"} element={<Navigate to={"/connexion"} replace/>}/>
+                <Route path={"/register"} element={<RegisterPage />} />
+                <Route path={"*"} element={<Navigate to={"/register"} replace/>}/>
             </Routes>
         </BrowserRouter>
     );
