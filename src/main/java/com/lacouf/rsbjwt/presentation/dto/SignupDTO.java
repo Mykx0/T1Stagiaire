89d55769d1142c1e898323afdc22bd68227aa869
edouard <ik_h291@outlook.com>
2026-09-15 12:39:25 -0400

    Ajout de la discipline dans le DTO

---
+++ src/main/java/com/lacouf/rsbjwt/presentation/dto/SignupDTO.java
@@ -0,0 +1,6 @@
+package com.lacouf.rsbjwt.presentation.dto;
+
+import com.lacouf.rsbjwt.model.Discipline;
+
+public record SignupDTO(String firstName, String lastName, String email, String password, Discipline discipline) {
+}
