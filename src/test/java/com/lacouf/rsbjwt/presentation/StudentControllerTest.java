ba16a29032d2e2d217b9310e834e5293e7005a2f
edouard <ik_h291@outlook.com>
2026-09-16 19:28:50 -0400

    Ajout du test courriel incorrect pour creation d'etudiant

--- src/test/java/com/lacouf/rsbjwt/presentation/StudentControllerTest.java
+++ src/test/java/com/lacouf/rsbjwt/presentation/StudentControllerTest.java
@@ -103,4 +103,27 @@ public class StudentControllerTest {
                 ).
                 andExpect(status().is4xxClientError());
     }
+
+    @Test
+    public void emailNotCorrectUnsuccessfulStudentCreation() throws Exception{
+        String signup =
+                """
+                {
+                    "firstName": "Edouard",
+                    "lastName": "Lambert",
+                    "email": "asdasd@",
+                    "password": "password",
+                    "discipline": "ComputerScience"
+                }
+                """;
+
+        this.mockMvc
+                .perform(
+                        post("/api/register/student")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(signup)
+                ).
+                andExpect(status().is4xxClientError());
+    }
 }
