dbedd490938f23461e32e4557968c3f174d59cf8
TNKDieguexPCLinux <diegoaguilar20040303@gmail.com>
2026-09-16 21:08:19 -0400

    Ajout du champ de discipline dans le formulaire d'inscription, mise à jour des labels et des valeurs des champs et ajout la traduction du français et anglais.

--- react-vite-project/src/components/auth/RegisterPage.jsx
+++ react-vite-project/src/components/auth/RegisterPage.jsx
@@ -1,25 +1,34 @@
 import React, {useState} from 'react';
 import { useTranslation } from 'react-i18next';
 import FormInputs from "../FormInputs.jsx";
+import axiosClient from "../../api/axiosClient.js";
 
 
 const RegisterPage = () => {
     const { t } = useTranslation();
 
+    const Discipline = {
+        ComputerScience: t('register.discipline.computerScience'),
+        Nursing: t("register.discipline.nursing"),
+        ElectricalEngineering: t("register.discipline.electricalEngineering")
+    }
+
     const INPUTS = [
-        {id: 1, label: t('register.inputs.lastName.label'), type: "text", placeholder: t('register.inputs.lastName.placeholder'), name: "nom"},
-        {id: 2, label: t('register.inputs.firstName.label'), type: "text", placeholder: t('register.inputs.firstName.placeholder'), name: "prenom"},
-        {id: 3, label: t('register.inputs.email.label'), type: "email", placeholder: t('register.inputs.email.placeholder'), name: "courriel"},
-        {id: 4, label: t('register.inputs.password.label'), type: "password", placeholder: "•••••", name: "motPasse"},
-        {id: 5, label: t('register.inputs.confirmPassword.label'), type: "password", placeholder: "•••••", name: "motPasseConfirmation"}
+        {id: 1, label: t('register.inputs.lastName.label'), type: "text", placeholder: t('register.inputs.lastName.placeholder'), name: "last_name"},
+        {id: 2, label: t('register.inputs.firstName.label'), type: "text", placeholder: t('register.inputs.firstName.placeholder'), name: "first_name"},
+        {id: 3, label: t('register.inputs.email.label'), type: "email", placeholder: t('register.inputs.email.placeholder'), name: "email"},
+        {id: 5, label: t('register.inputs.discipline.label'), type: "select", placeholder: t('register.inputs.discipline.placeholder'), name: "discipline", options: Object.values(Discipline)},
+        {id: 6, label: t('register.inputs.password.label'), type: "password", placeholder: "•••••", name: "password"},
+        {id: 7, label: t('register.inputs.confirmPassword.label'), type: "password", placeholder: "•••••", name: "passwordConfirmation"}
     ];
 
     const [values, setValues] = useState({
-        nom: '',
-        prenom: '',
-        courriel: '',
-        motPasse: '',
-        motPasseConfirmation: ''
+        last_name: '',
+        first_name: '',
+        email: '',
+        discipline: Discipline.ComputerScience,
+        password: '',
+        passwordConfirmation: ''
     });
 
     const [error, setError] = useState('');
@@ -36,7 +45,7 @@ const RegisterPage = () => {
             setError('register.errors.allFieldsRequired');
             return false;
         }
-        if(values.nom.length < 1 || values.prenom.length < 1){
+        if(values.last_name.length < 1 || values.first_name.length < 1){
             setError('register.errors.nameMinLength');
             return false;
         }
@@ -44,7 +53,7 @@ const RegisterPage = () => {
             setError('register.errors.invalidEmail');
             return false;
         }
-        if (values.motPasse.length < 8){
+        if (values.password.length < 8){
             setError('register.errors.passwordMinLength');
             return false;
         }
@@ -55,14 +64,14 @@ const RegisterPage = () => {
         return true;
     };
     const champsComplets = () => {
-        return values.nom !== '' && values.prenom !== '' && values.matricule !== '' && values.courriel !== '' && values.motPasse !== '' && values.motPasseConfirmation !== '';
+        return values.last_name !== '' && values.first_name !== '' && values.email !== '' && values.discipline !== '' && values.password !== '' && values.passwordConfirmation !== '';
     }
     const validateEmail = () => {
         const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-        return emailRegex.test(values.courriel);
+        return emailRegex.test(values.email);
     }
     const motPasseCompatible = () => {
-        return values.motPasse === values.motPasseConfirmation;
+        return values.password === values.passwordConfirmation;
     }
 
     const handleRegister = async (e) => {
@@ -72,21 +81,23 @@ const RegisterPage = () => {
         try{
             setDisableButton(true);
             const data = {
-                nom: values.nom,
-                prenom: values.prenom,
-                courriel: values.courriel,
-                motPasse: values.motPasse
+                last_name: values.last_name,
+                first_name: values.first_name,
+                email: values.email,
+                discipline: values.discipline,
+                password: values.password
             };
-            await new Promise(resolve => setTimeout(resolve, 2000));
+            const response = await axiosClient.post('/register/student', data);
             console.log(data)
             setValues({
-                nom: '',
-                prenom: '',
-                courriel: '',
-                motPasse: '',
-                motPasseConfirmation: ''
+                last_name: '',
+                first_name: '',
+                email: '',
+                discipline: Discipline.ComputerScience,
+                password: '',
+                passwordConfirmation: ''
             });
-            setSimulationResponse( `${t('register.successMessage')}\n${JSON.stringify(data)}`);
+            setSimulationResponse( await axiosClient.get(`/${response.data.id}`));
         }catch(e){
             setDisableButton(false);
             setError('register.errors.genericError');
@@ -95,9 +106,7 @@ const RegisterPage = () => {
         }
     }
 
-
     return (
-
             <div>
                 <div className="text-center">
                     <h1 className="title">{t('register.title')}</h1>
@@ -112,6 +121,7 @@ const RegisterPage = () => {
                             type={input.type}
                             value={values[input.name]}
                             onChange={onChange}
+                            options={input.options}
                             name={input.name}
                             label={input.label}
                       />
